6e2002b9fb4cf0bfef501e3761e3dc1e435708a8
confett0 <118047961+confett0@users.noreply.github.com>
2023-02-03 18:07:14 +0100

    Create functions that allow players to add marks to a specific spot on the board

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
 // Tic Tac Toe
 
 const gameboard = {
-    gameSlot: [null,null,null,null,null,null,null,null,null,]
-};
+    gameSlot: [null,null,null,null,null,null,null,null,null,],
+    init () {
+        this.cacheDom();
+        this.bindEvents();
+    },
+    cacheDom () {
+        this.slot = document.querySelectorAll(".gameboard-slot");
+    },
+    bindEvents() {
+        this.slot.forEach(element => {
+            element.addEventListener("click", (e) => {
+                if (player1Turn) {
+                this.gameSlot[e.target.id] = player1.symbol;
+            } else {
+                this.gameSlot[e.target.id] = player2.symbol;
+            }
+                this.arrayToGameboard();
+                gameFlow.switchPlayerTurn();
+            })
+        });
+    },
+    arrayToGameboard() {
+        let i = 0
+        while (i < this.gameSlot.length) {
+            this.slot.forEach(element => {
+                element.textContent = this.gameSlot[i];
+                i++;
+        })
+}}};
 
-const Player = (name,symbol) {
-    return {name, symbol};
-};
+gameboard.init();
+
+const Player = (name,symbol) => ({name, symbol});
 
 const player1 = Player("player1", "x");
 const player2 = Player("player2", "o");
+
+let player1Turn = true;
+let player2Turn = false;
+
+const gameFlow = {
+    switchPlayerTurn() {
+        
+        if (player1Turn) {
+            player2Turn = true;
+            player1Turn = false;
+        } else {
+            player1Turn = true;
+            player2Turn = false;
+        }
+    }
+};

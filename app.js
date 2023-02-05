0eec9bc4c6be1934146eb7188bbf7f21b0619eee
confett0 <118047961+confett0@users.noreply.github.com>
2023-02-05 11:40:08 +0100

    Add move counter

--- app.js
+++ app.js
@@ -1,58 +1,63 @@
-// Tic Tac Toe
-
-const gameboard = {
-    gameSlot: [null,null,null,null,null,null,null,null,null,],
-    init () {
-        this.cacheDom();
-        this.bindEvents();
-    },
-    cacheDom () {
-        this.slot = document.querySelectorAll(".gameboard-slot");
-    },
-    bindEvents() {
-        this.slot.forEach(element => {
-            element.addEventListener("click", (e) => {
-                if (this.gameSlot[e.target.id] !== null) {
-                    return;
-                }
-                if (player1Turn) {
-                this.gameSlot[e.target.id] = player1.symbol;
-            } else {
-                this.gameSlot[e.target.id] = player2.symbol;
-            }
-                this.arrayToGameboard();
-                gameFlow.switchPlayerTurn();
-            })
-        });
-    },
-    arrayToGameboard() {
-        let i = 0
-        while (i < this.gameSlot.length) {
-            this.slot.forEach(element => {
-                element.textContent = this.gameSlot[i];
-                i++;
-        })
-}}};
-
-gameboard.init();
-
-const Player = (name,symbol) => ({name, symbol});
+const Player = (name, symbol) => ({ name, symbol });
 
 const player1 = Player("player1", "x");
 const player2 = Player("player2", "o");
 
-let player1Turn = true;
-let player2Turn = false;
+const gameboard = {
+  board: [null, null, null, null, null, null, null, null, null],
+  init() {
+    this.cacheDom();
+    this.bindEvents();
+  },
+  cacheDom() {
+    this.slot = document.querySelectorAll(".gameboard-slot");
+  },
+  bindEvents() {
+    this.slot.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        if (this.board[e.target.id] !== null) {
+          return;
+        }
+        if (gameFlow.turn === "player1") {
+          this.board[e.target.id] = player1.symbol;
+        } else {
+          this.board[e.target.id] = player2.symbol;
+        }
+        gameFlow.moveCounter++;
+        this.arrayToGameboard();
+        gameFlow.switchPlayerTurn();
+        gameFlow.winGame();
+      });
+    });
+  },
+  arrayToGameboard() {
+    let i = 0;
+    while (i < this.board.length) {
+      this.slot.forEach((element) => {
+        element.textContent = this.board[i];
+        i++;
+      });
+    }
+  }
+};
+
+gameboard.init();
 
 const gameFlow = {
-    switchPlayerTurn() {
-        
-        if (player1Turn) {
-            player2Turn = true;
-            player1Turn = false;
-        } else {
-            player1Turn = true;
-            player2Turn = false;
-        }
-    }
+  moveCounter: 0,
+  turn: "player1",
+  switchPlayerTurn() {
+    this.turn === "player1" ? this.turn = "player2" : this.turn = "player1";
+  },
+
+  winGame() {
+    if (
+      gameboard.board[0] ===
+      gameboard.board[1] &&
+      gameboard.board[1] ===
+      gameboard.board[2]
+    ) {
+      console.log("you win");
+    } 
+  },
 };

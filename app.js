29117931f4d8ba60bb0358dbf544098e7a8c6fc9
confett0 <118047961+confett0@users.noreply.github.com>
2023-02-07 14:15:14 +0100

    Refactor code using module pattern

--- app.js
+++ app.js
@@ -3,71 +3,26 @@ const Player = (name, symbol) => ({ name, symbol });
 const player1 = Player("player1", "x");
 const player2 = Player("player2", "o");
 
-const gameboard = {
-  board: [null, null, null, null, null, null, null, null, null],
-  init() {
-    this.cacheDom();
-    this.bindEvents();
-  },
-  cacheDom() {
-    this.slot = document.querySelectorAll(".gameboard-slot");
-  },
-  bindEvents() {
-    this.slot.forEach((element) => {
-      element.addEventListener("click", this.play);
-    });
-  },
+const display = document.querySelector(".display");
+const restart = document.querySelector(".restart");
 
-  arrayToGameboard() {
-    // assign array elements to board
-    let i = 0;
-    while (i < this.board.length) {
-      this.slot.forEach((element) => {
-        element.textContent = this.board[i];
-        i++;
-      });
-    }
-  },
+const game = (() => {
+  let winner = "";
+  let moveCounter = 0;
+  let currentPlayer = "player1";
 
-  play(e) {
-    if (gameboard.board[e.target.id] !== null) {
-      return;
-    }
-    if (gameFlow.currentPlayer === "player1") {
-      gameboard.board[e.target.id] = player1.symbol;
+  const switchPlayerTurn = () => {
+    if (currentPlayer === "player1") {
+       currentPlayer = "player2";
+       display.textContent = "Player 2's turn";
     } else {
-      gameboard.board[e.target.id] = player2.symbol;
+      currentPlayer = "player1";
+      display.textContent = "Player 1's turn";
     }
-    gameFlow.moveCounter++;
-    gameboard.arrayToGameboard();
-    gameFlow.switchPlayerTurn();
-    gameFlow.endGame();
-  },
-};
+    return currentPlayer;
+  };
 
-gameboard.init();
-
-const gameFlow = {
-  winner: "",
-  moveCounter: 0,
-  currentPlayer: "player1",
-  switchPlayerTurn() {
-    this.currentPlayer === "player1"
-      ? (this.currentPlayer = "player2")
-      : (this.currentPlayer = "player1");
-  },
-
-  endGame() {
-    if (this.moveCounter >= 5) {
-      this.checkWinner();
-      // check for draw
-      if (this.moveCounter === 9 && this.winner === undefined) {
-        this.winner = "draw";
-      }
-    }
-  },
-
-  winLines: [
+  const winLines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -76,31 +31,95 @@ const gameFlow = {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ],
+  ];
 
-  checkWinner() {
-    for (let i = 0; i < 7; i++) {
-      const winCondition = this.winLines[i];
+  const checkWinner = () => {
+    for (let i = 0; i <= 7; i++) {
+      const winCondition = winLines[i];
       const a = gameboard.board[winCondition[0]];
       const b = gameboard.board[winCondition[1]];
       const c = gameboard.board[winCondition[2]];
 
-      if (a === null || b === null || c === null) {
+      if (a === "" || b === "" || c === "") {
         continue;
       }
       if (a === b && b === c) {
-        this.winner = a;
+        winner = a;
         break;
       }
     }
-    console.log(this.winner);
-    return this.winner;
-  },
+    return winner;
+  }
 
-  resetGame() {
-    gameboard.board = [null, null, null, null, null, null, null, null, null];
-    this.moveCounter = 0;
-    this.currentPlayer = "player1";
-    this.winner = "";
-  },
-};
+  const endGame = () => {
+    if (winner === "x") {
+      display.textContent = "Player 1 wins!";
+    } else if (winner === "o") {
+      display.textContent = "Player 2 wins!";
+    } else if (winner === "draw") {
+      display.textContent = "It's a draw!";
+    }
+  };
+
+  const play = (e) => {
+    if (gameboard.board[e.target.id] !== "" || winner !== "") {
+      return;
+    };
+    moveCounter++;
+    if (moveCounter === 9 && winner === "") {
+      winner = "draw";
+    };
+    if (currentPlayer === "player1") {
+      gameboard.board[e.target.id] = player1.symbol;
+    } else {
+      gameboard.board[e.target.id] = player2.symbol;
+    };
+    gameboard.arrayToGameboard();
+    switchPlayerTurn();
+    checkWinner();
+    endGame();
+  };
+
+  const reset = () => {
+    winner = "";
+    moveCounter = 0;
+    currentPlayer = "player1";
+  }
+  return { winner, moveCounter, currentPlayer, switchPlayerTurn, play, checkWinner, reset };
+
+})();
+
+const gameboard = (() => {
+
+  const board = ["", "", "", "", "", "", "", "", ""];
+  const cell = document.querySelectorAll(".gameboard-cell");
+
+  const arrayToGameboard = () => {
+    // display array elements in gameboard
+    for (let i = 0; i < 9; i++) {
+      cell[i].textContent = board[i];
+    }
+  };
+
+  cell.forEach((box) => {
+    box.addEventListener("click", game.play);
+  });
+
+  const reset = () => {
+    for (let i = 0; i < 9; i++) {
+      board[i] = "";
+    }
+    arrayToGameboard();
+  };
+
+  return {
+    board, arrayToGameboard, reset
+  };
+})();
+
+
+restart.addEventListener("click", () => {
+    game.reset();
+    gameboard.reset();
+    display.textContent = "Player 1, make your move!";
+  });
